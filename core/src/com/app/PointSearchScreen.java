08dc4a3c17a37e7ea5ad2d05b57a0cc64c0a816f
Raul0800 <36411548+Raul0800@users.noreply.github.com>
2018-04-01 18:29:25 +0300

    Add switching between floors
    Add switching between floors with button. Fix texture.
    #PM-59 state to verify

--- core/src/com/app/PointSearchScreen.java
+++ core/src/com/app/PointSearchScreen.java
@@ -4,7 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
@@ -37,6 +39,21 @@ public class PointSearchScreen extends Stage implements Screen {
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
         Skin skin = new Skin(Gdx.files.internal("skin/glassy-ui.json"));
+
+        //Change colour
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.GREEN);
+        pixmap.fill();
+        BitmapFont myFont = new BitmapFont();
+        myFont.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        myFont.getData().setScale(2,2);
+        skin.add("green", new Texture(pixmap));
+        skin.add("default", myFont);//new BitmapFont());
+
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("green", new Color((float)0.42, (float)0.71, (float)0.27, 1));
+        textButtonStyle.font = skin.getFont("default");
+        skin.add("default", textButtonStyle);
 
         //Text Button "BACK"
         TextButton backButton = new TextButton("< BACK", skin, "default");
